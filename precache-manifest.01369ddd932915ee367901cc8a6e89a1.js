f447cb1c273d83bd1018a1da520a1ffbeb37633e
bens <bens@spreadshirt.net>
2019-02-07 21:50:23 +0100

    Updates

---
+++ precache-manifest.01369ddd932915ee367901cc8a6e89a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/css-chainsaw/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "596b64ed62e125ca6192",
+    "url": "/css-chainsaw/static/js/runtime~main.596b64ed.js"
+  },
+  {
+    "revision": "8731d0d35b93be85d863",
+    "url": "/css-chainsaw/static/js/main.8731d0d3.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/css-chainsaw/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "8731d0d35b93be85d863",
+    "url": "/css-chainsaw/static/css/main.8aadd2fb.chunk.css"
+  },
+  {
+    "revision": "ff993bcecf3c2ac1bdeeec616076395f",
+    "url": "/css-chainsaw/index.html"
+  }
+];
